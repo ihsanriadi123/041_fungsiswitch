a357befff40ac2b94033f85f07bdcaf279c964f3
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-03-25 15:14:46 +0700

    membuat perulangan do while

--- calculator.cpp
+++ calculator.cpp
@@ -33,3 +33,15 @@ float pengurangan(float a, float b) {
 int main() {
 
     int pilihan;
+
+do {
+        cout << "=========================" << endl;
+        cout << "Menu" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pembagian" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pengurangan" << endl;
+        cout << "5. exit" << endl;
+        cout << "Masukkan pilihan 1/2/3/4/5 = ";
+        cin >> pilihan;
+        
