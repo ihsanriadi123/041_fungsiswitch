4ee0dbe8a9d3a39bebecb49fc85944aad5c6bd3a
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-03-25 15:12:14 +0700

    membuat data dari bilangan satu dan dua

--- calculator.cpp
+++ calculator.cpp
@@ -2,3 +2,11 @@
 using namespace std;
 
 int bilsatu, bildua;
+
+
+void input() {
+    cout << "masukkan bilangan pertama = ";
+    cin >> bilsatu;
+    cout << "Masukkan bilangan kedua = ";
+    cin >> bildua;
+}
