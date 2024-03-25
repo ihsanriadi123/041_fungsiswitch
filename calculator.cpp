341f91d3f8cabed6e80672a1e734ef3b777c2874
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-03-25 15:13:00 +0700

    membuat bagian dari float penjumlahan,pengurangan, pembagian, perkalian

--- calculator.cpp
+++ calculator.cpp
@@ -10,3 +10,22 @@ void input() {
     cout << "Masukkan bilangan kedua = ";
     cin >> bildua;
 }
+
+float penjumlahan(float a, float b) {
+    return a + b;
+}
+
+
+float pembagian(float a, float b) {
+    return a / b;
+
+}
+
+float perkalian(float a, float b) {
+    return a * b;
+
+}
+
+float pengurangan(float a, float b) {
+    return a - b;
+}
